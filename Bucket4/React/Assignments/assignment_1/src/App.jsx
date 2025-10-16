aed178499ce7851be68880309efd30cfb0889a3f
ESET0134 <shruti.singhania@esyasoft.com>
2025-10-16 16:32:57 +0530

    Changes

--- Bucket4/React/Assignments/assignment_1/src/App.jsx
+++ Bucket4/React/Assignments/assignment_1/src/App.jsx
@@ -22,8 +22,8 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
-      <h1 className="text-2xl text-black font-bold mb-6">📊 Product Sales Distribution</h1>
-      <div className="w-full max-w-xl h-96 bg-white rounded-2xl shadow-lg p-4">
+      <h1 className="text-2xl text-black font-bold">Product Sales Distribution</h1>
+      <div className="w-full max-w-xl h-96 rounded-2xl">
         <ResponsiveContainer>
           <PieChart>
             <Pie
@@ -33,7 +33,6 @@ function App() {
               cx="50%"
               cy="50%"
               outerRadius={120}
-              fill="#8884d8"
               label
             >
               {data.map((entry, index) => (
