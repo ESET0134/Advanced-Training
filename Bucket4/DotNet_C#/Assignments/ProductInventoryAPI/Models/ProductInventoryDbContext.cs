ac73f81a6e5fcb73ee19c40c57e5270421d6f80c
ESET0134 <shruti.singhania@esyasoft.com>
2025-10-24 11:32:23 +0530

    .NET Assignment2

---
+++ Bucket4/DotNet_C#/Assignments/ProductInventoryAPI/Models/ProductInventoryDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductInventoryAPI.Models;
+
+public partial class ProductInventoryDbContext : DbContext
+{
+    public ProductInventoryDbContext()
+    {
+    }
+
+    public ProductInventoryDbContext(DbContextOptions<ProductInventoryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-08859FSJ;Initial Catalog=ProductInventoryDB;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A0BD12D145B");
+
+            entity.Property(e => e.CategoryName).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6CD1ED40E34");
+
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ProductName).HasMaxLength(150);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Products_Categories");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Bucket4/DotNet_C#/Assignments/ProductInventoryAPI/Repository/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProductInventoryAPI.Models;
+
+namespace ProductInventoryAPI.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>
+    {
+        public CategoryRepository(ProductInventoryDbContext context) : base(context)
+        {
+        }
+    }
+}
